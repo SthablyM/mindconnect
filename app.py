3b435bac7d9b452fb73049a751cc2098f989479e
SthablyM <mathapelofentse@icloud.com>
2024-09-03 22:07:12 +0200

    mindconnect

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from datetime import timezone
-from flask import Flask, render_template, request
-from db import db
+from flask import Flask, redirect, render_template, request, session
+from db import User, db
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:root@localhost:3306/mindconnect"
+app.secret_key = 'hgaefdhfaevevyatedv'
 
 db.init_app(app)
 
@@ -14,13 +15,92 @@ with app.app_context():
 def home():
     return render_template("home.html")
 
-@app.route('/signin', methods=["GET"])
+@app.route('/signin', methods=["POST", "GET"])
 def signin():
-    return render_template("login.html")
+    if (request.method == "GET"):
+        return render_template("login.html", message = "")   
+    else:
+        email = request.form["email"]
+        password = request.form["password"]
+        existing_user = User.query.filter_by(email=email, password = password).first()
+        if (existing_user is not None):
+            # session["logged_in"] = "success"
+            return redirect('/feeds')
+        else: 
+            return render_template("login.html", message = "Invalid login details") 
         
-
-@app.route('/signup', methods=["GET"])
+@app.route('/signup', methods=["POST", "GET"])
 def signup():
-    return render_template("signup.html")
+    if (request.method == "GET"):
+        return render_template("signup.html")
+    else:
+        first_name = request.form["first_name"]
+        last_name = request.form["last_name"]
+        phone_number = request.form["phone_number"]        
+        email = request.form["email"]
+        password = request.form["password"]
+        confirm_password = request.form["confirm_password"]
+        
+        if (first_name == "" or email == "" or password == "" or confirm_password == ""):
+            return render_template('signup.html')
+        if (password != confirm_password):
+            return render_template('signup.html')
+        else: 
+            new_user = User(
+                first_name = first_name,
+                last_name = last_name,
+                phone_number = phone_number,
+                email = email,
+                password = password
+            )
+            db.session.add(new_user)
+            db.session.commit()
+            if (new_user.id > 0):
+                return redirect("/signin")
+            else: 
+                return render_template('signup.html')
+    
+@app.route('/forgot-password', methods=["POST", "GET"])
+def forgotPassword():
+    if (request.method == "GET"):
+        return render_template("forgotpassword.html")  
+    else:
+        email = request.form["email"]
+        if (email == ""):
+            return render_template('forgotpassword.html')
+        else: 
+            existing_user = User.query.filter_by(email=email).first()
+            if (existing_user is not None):
+                session["email"] = email
+                return redirect("/reset-password")
+            else:
+                return render_template("forgotpassword.html", message = "Invalid email address")      
+    
+@app.route('/reset-password', methods=["POST", "GET"])
+def restPassword():
+    if (request.method == "GET"):
+        return render_template("resetpassword.html")  
+    else:
+        password = request.form["password"]
+        confirm_password = request.form["confirm_password"]
+        if (password == "" or confirm_password == ""):
+            return render_template('resetpassword.html')
+        if (password != confirm_password):
+            return render_template('resetpassword.html')
+        else: 
+            email = session["email"]
+            existing_user = User.query.filter_by(email=email).first()
+            if (existing_user is not None):
+                existing_user.password = password
+                db.session.add(existing_user)
+                db.session.commit()
+            return render_template("login.html")     
+
+
+@app.route('/feeds', methods=["POST", "GET"])
+def feeds():
+    if (request.method == "GET"):
+        return render_template("feeds.html")  
+             
 
 app.run(debug=True)
--- db.py
+++ db.py
@@ -1,4 +1,4 @@
-from datetime import timezone
+from datetime import datetime, timezone
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
@@ -12,8 +12,8 @@ class User(db.Model):
     phone_number = db.Column(db.String(16))
     email = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(500), unique=False, nullable=False)
-    created_at = db.Column(db.DateTime, default=timezone.utc)
-    updated_at = db.Column(db.DateTime, default=timezone.utc)  
+    created_at = db.Column(db.DateTime, default=datetime.now())
+    updated_at = db.Column(db.DateTime, default=datetime.now())  
 
 class Post(db.Model):
     __tablename__ = "posts"
@@ -22,11 +22,11 @@ class Post(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
     post = db.Column(db.String(500), nullable=False)
     image_uri = db.Column(db.String(500))
-    created_at = db.Column(db.DateTime, default=timezone.utc)
-    updated_at = db.Column(db.DateTime, default=timezone.utc)  
+    created_at = db.Column(db.DateTime, default=datetime.now())
+    updated_at = db.Column(db.DateTime, default=datetime.now())  
 
-    comments = db.relationship("PostComment", back_populates="post", lazy="dynamic")
-    likes = db.relationship("PostLike", back_populates="post", lazy="dynamic")     
+    # comments = db.relationship("PostComment", back_populates="post", lazy="dynamic")
+    # likes = db.relationship("PostLike", back_populates="post", lazy="dynamic")     
 
 class PostComment(db.Model):
     __tablename__ = "post_comments"
@@ -35,8 +35,8 @@ class PostComment(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
     post_id = db.Column(db.Integer, db.ForeignKey("posts.id"), nullable=False)
     comment = db.Column(db.String(500), nullable=False)
-    created_at = db.Column(db.DateTime, default=timezone.utc)
-    updated_at = db.Column(db.DateTime, default=timezone.utc) 
+    created_at = db.Column(db.DateTime, default=datetime.now())
+    updated_at = db.Column(db.DateTime, default=datetime.now()) 
 
 class PostLikes(db.Model):
     __tablename__ = "post_likes"
@@ -44,8 +44,8 @@ class PostLikes(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
     post_id = db.Column(db.Integer, db.ForeignKey("posts.id"), nullable=False)
-    created_at = db.Column(db.DateTime, default=timezone.utc)
-    updated_at = db.Column(db.DateTime, default=timezone.utc)  
+    created_at = db.Column(db.DateTime, default=datetime.now())
+    updated_at = db.Column(db.DateTime, default=datetime.now())  
 
 class MoodLog(db.Model):
     __tablename__ = "moods_log"
@@ -54,5 +54,5 @@ class MoodLog(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
     mood_question = db.Column(db.String(500))
     mood_answer = db.Column(db.String(500))
-    created_at = db.Column(db.DateTime, default=timezone.utc)
-    updated_at = db.Column(db.DateTime, default=timezone.utc)      
+    created_at = db.Column(db.DateTime, default=datetime.now())
+    updated_at = db.Column(db.DateTime, default=datetime.now())      
